b6a763298c2c9f0aa6dba5fae2483ec29f198827
Anuj Bansal <anuj.bansal@students.iiit.ac.in>
2018-10-29 00:35:00 +0530

    Sed Implementation

--- Question2/pyshell.py
+++ Question2/pyshell.py
@@ -1,4 +1,5 @@
 import os
+import re
 #for printing formatted output without newline
 import sys
 
@@ -12,6 +13,8 @@ def getListOfFiles(dirName):
 def printList(mylist):
 	for it in mylist:		
 		print("\t"+it)
+
+
 
 def headF(keywords,length):
 	#default read 10 lines
@@ -34,6 +37,23 @@ def headF(keywords,length):
 	for x in range(numLineORChar):
 		sys.stdout.write(f.readline())
 	return 1
+
+def trF(keywords, length):
+	f=open(keywords[length-1], "r")
+	options=keywords[1]
+	if options=="-d":
+		reg=keywords[2]
+		reg=re.compile(reg)
+		for line in f:
+			sys.stdout.write(reg.sub('',line))
+	else:
+		reg=options
+		res=keywords[2]
+		for line in f:
+			sys.stdout.write(line.translate(reg,res))
+
+
+
 
 def tailF(keywords,length):
 	#default read 10 lines
@@ -73,20 +93,67 @@ def grepF(keywords, length):
 	elif keywords[1]=="-i":
 		f = open(keywords[3], "r")
 		option=keywords[1]
-		searchWord=(keywords[2]).lower()
+		searchWord=(keywords[2])
 	else:
 		return 1
+	if option is not None:
+		pattern=re.compile(searchWord,re.IGNORECASE)
+	else:
+		pattern=re.compile(searchWord)
 	for line in f:
-		if option is not None:
-			if searchWord in line.lower():
-				sys.stdout.write(line)
+		if pattern.search(line):
+			sys.stdout.write(line)
+
+
+def replacenth(string, sub, wanted, n):
+    where = [m.start() for m in re.finditer(sub, string)]
+    if len(where)<n:
+    	return string
+    where=where[n-1]
+    before = string[:where]
+    after = string[where:]
+    after = re.sub(sub, wanted,after,1)
+    newString = before + after
+    return newString
+
+def sedF(keywords,length):
+	f = open(keywords[length-1], "r")
+	sedCmd=keywords[length-2]
+	sedCmd=sedCmd.split("/")
+	if len(sedCmd)==4:
+		if sedCmd[0]=="s":
+			patt=sedCmd[1]
+			res=sedCmd[2]
+			options=sedCmd[3]
+			reg=re.compile(patt)
+			if len(options)>0:
+				if "i" in options:
+					reg=re.compile(patt, re.IGNORECASE)
+			for line in f:
+				replaced=line
+				if len(options)>0:
+					if "g" in options:
+						replaced=reg.sub(res, line)
+					else:
+						if options[0].isdigit():
+							replaced=replacenth(line,patt,res,int(options[0]))
+						else:
+							replaced=reg.sub(res,line,1)		
+				else:
+					replaced=reg.sub(res,line,1)
+					# re.sub(^((.*?reg.*?){1})reg, "\1"+res, line)
+					
+				sys.stdout.write(replaced)
 		else:
-			if searchWord in line:
-				sys.stdout.write(line)
+			print("Erro: Not valid command")
+	else:
+		print("Error: Invalid Command")
+
+
 
 def execCommand(keywords):
 	if(len(keywords)>1 and keywords[0]=="touch"):
-		print("Toucin a file")
+		# print("Toucin a file")
 		try:
 			f= open(keywords[1],"w+")
 			f.close();
@@ -135,6 +202,20 @@ def execCommand(keywords):
 		except Exception as e:
 			print(e)
 		return 1	
+
+	elif((len(keywords)>=3)  and keywords[0]=="tr"):
+		try:
+		 	trF(keywords,len(keywords))
+		except Exception as e:
+			print(e)
+		return 1	
+
+	elif((len(keywords)==3)  and keywords[0]=="sed"):
+		try:
+		 	sedF(keywords,len(keywords))
+		except Exception as e:
+			raise e
+		return 1	
 	
 	elif(keywords[0]=="exit"):
 		return 0
@@ -152,4 +233,4 @@ os.system('clear')
 while 1==1:
 	print("Pyshell: "+pwd())
 	if(not(main())):
-		break
+		break;
